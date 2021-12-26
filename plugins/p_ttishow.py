e97a22e724dfc7da71d9782f28839101ee60ae44
SuryaPrabhas1245 <85777749+SuryaPrabhas1245@users.noreply.github.com>
2021-12-26 07:27:24 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -22,7 +22,7 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             # Inspired from a boat of a banana tree
             buttons = [[
-                InlineKeyboardButton('Support', url=f'https://t.me/TeluguDubbedHorrorMovies2')
+                InlineKeyboardButton('Support ⚡', url=f'https://t.me/suryaprabhas1245')
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             k = await message.reply(
@@ -65,7 +65,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/TeluguDubbedHorrorMovies2')
+            InlineKeyboardButton('Support ⚡', url=f'https://t.me/suryaprabhas1245')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
@@ -103,7 +103,7 @@ async def disable_chat(bot, message):
     await message.reply('Chat Succesfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/TeluguDubbedHorrorMovies2')
+            InlineKeyboardButton('Support ⚡', url=f'https://t.me/suryaprabhas1245')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
